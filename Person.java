ab7b51240fa6eca8b0fb5699ae69eb80395e2081
Carson169 <carsonjsamples@icloud.com>
2026-09-16 19:30:50 -0400

    Complete Week 2 Lab 2 Part A

--- Person.java
+++ Person.java
@@ -1,3 +1,41 @@
-public class Person {
-    
+//Carson Samples
+//Lab Assignment 4A
+//9.16.26
+//Course CMP-129-80231
+public class Person 
+{
+    private String name;
+    private double age;
+    private String email;
+    public String getName() 
+    {
+        return name;
+    }
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+    public double getAge() 
+    {
+        return age;
+    }
+    public void setAge(double age) 
+    {
+        this.age = age;
+    }
+    public String getEmail() 
+    {
+        return email;
+    }
+    public void setEmail(String email) 
+    {
+        this.email = email;
+    }
+    public void displayInfo() 
+    {
+        System.out.println("Person Information");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email + "\n");
+    }
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,21 @@
-public class PersonTest {
-    
+//Carson Samples
+//Lab Assignment 4A
+//9.16.26
+//Course CMP-129-80231
+public class PersonTest 
+{
+    public static void main(String[] args) 
+    {
+        Person person1 = new Person();
+        Person person2 = new Person();
+        person1.setName("Maria Johnson");
+        person1.setAge(28);
+        person1.setEmail("maria@example.com");
+        person2.setName("David Smith");
+        person2.setAge(35);
+        person2.setEmail("david@example.com");
+        System.out.println("Retrieving first person's name: " + person1.getName() + "\n");
+        person1.displayInfo();
+        person2.displayInfo();
+    }  
 }
